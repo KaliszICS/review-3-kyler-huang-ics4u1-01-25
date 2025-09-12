1d161ffe809bfc23cdf675c88bed85da58b9ac7a
kyler-huang-ics4u1-01-25 <kyler.huang@student.tdsb.on.ca>
2025-09-12 18:50:36 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,47 +3,72 @@ import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		q1();
-		q2();
-		q3();
-		q4();
-		q5();
-		q6();
-		q7();
+		Scanner scanner = new Scanner(System.in);
+		q1(scanner);
+		q2(scanner);
+		q3(scanner);
+		q4(scanner);
+		q5(scanner);
+		q6(scanner);
+		q7(scanner);
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static void q1(Scanner scanner) {
+		System.out.println("In: ");
+		String word_1 = scanner.nextLine();
+		char first_character = word_1.charAt(0);
+
+		System.out.println(first_character);
+
+	}
+
+	public static void q2(Scanner scanner) {
+		System.out.println("In: ");
+		boolean opposite_bool = scanner.nextBoolean();
+		System.out.println(!opposite_bool);
 		
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static void q3(Scanner scanner) {
+		scanner.nextLine();
+		System.out.println("In: ");
+		int num_1 = scanner.nextInt();
+		System.out.println(num_1 > 5);
 		
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static void q4(Scanner scanner) {
+		scanner.nextLine();
+		System.out.println("In: ");
+		double double_1 = scanner.nextDouble();
+    	System.out.println(double_1 >= -2 && double_1 <= 2);
 		
 	}
 
-	public static void q4() {
-		//Write question 4 code here
+	public static void q5(Scanner scanner) {
+		scanner.nextLine();
+		System.out.println("In: ");
+		String hello_world = scanner.nextLine();
+		System.out.println(hello_world.equals("Hello World"));
 		
 	}
 
-	public static void q5() {
-		//Write question 5 code here
+	public static void q6(Scanner scanner) {
+		System.out.println("In: ");
+		int num_2 = scanner.nextInt();
+		int num_3 = scanner.nextInt();
+		System.out.println(num_2<=num_3);
+
 		
 	}
 
-	public static void q6() {
-		//Write question 6 code here
-		
-	}
+	public static void q7(Scanner scanner) {
+		scanner.nextLine();
+		System.out.println("In: ");
+		double double_2 = scanner.nextDouble();
+		double double_3 = scanner.nextDouble();
+		System.out.println(double_2>double_3);
 
-	public static void q7() {
-		//Write question 7 code here
 		
 	}
 
